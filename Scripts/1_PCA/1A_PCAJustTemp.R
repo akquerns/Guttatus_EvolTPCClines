9873fca26f96095e85baf10fb7414f8e391a570b
akquerns <akquerns@ncsu.edu>
2020-09-07 16:36:45 -0400

    Edits to PCA--include vectors for variables

--- Scripts/1_PCA/1A_PCAJustTemp.R
+++ Scripts/1_PCA/1A_PCAJustTemp.R
@@ -14,6 +14,9 @@ library(FactoMineR)
 library(maps)
 library(mapdata)
 library(tidyr)
+library(broom)
+library(knitr)
+library(ggfortify)
 
 #Read in files. Rename them.
 
@@ -38,17 +41,21 @@ mg_wcTEMP <- read_csv("Raw-data/guttatus_worldclim_seed_collections.csv")
 View(mg_wcTEMP)
 
 ## remove variables unnecessary for TEMPERATURE PCA and rows with NA ##
-mg_wcTEMP <- subset(mg_wcTEMP, select = -c(bio_12, bio_13, bio_14, bio_15, bio_16, bio_17, bio_18, bio_19))
+mg_wcTEMP <- subset(mg_wcTEMP, select = -c(bio_12, bio_13, bio_14, bio_15, bio_16, bio_17, bio_18, bio_19, Longitude, Latitude))
+
 mg_wcTEMP <- na.omit(mg_wcTEMP)
-mg_wcTEMP2 <- subset(mg_wcTEMP, select = -c(Longitude, Latitude))
-mg_wcTEMP3 <- subset(mg_wcTEMP2, select = -c(ID1))
+mg_wcTEMP2 <- subset(mg_wcTEMP, select = -c(ID1))
+
+##change column names to abbreviated variable names##
+names(mg_wcTEMP) <- c("Region", "Population", "MAT", "MDR", "ISO", "TS", "MTWM", "MTCM", "TAR", "MTWQ", "MTDQ", "MTWAQ", "MTCQ")
+
 
 #################################
 ## 2. run PCA using FactoMineR ##
 #################################
 library(FactoMineR)
 library(multcompView)
-mg_pcaTEMP <- PCA(mg_wcTEMP3, scale.unit=TRUE, quali.sup = 1, graph = FALSE)
+mg_pcaTEMP <- PCA(mg_wcTEMP2, scale.unit=TRUE, quali.sup = 1, graph = FALSE)
 
 ## (optional) view PCA summary and variable and individual  ##
 ## values(coordinates, correlation, cosine2, contribuytion) ##
@@ -69,9 +76,31 @@ mg_wcTEMP$pc2 <- mg_pcaTEMP$ind$coord[,2]
 library(ggrepel)
 
 ## create PCA plot, point color by country ##
-ggplot(data=mg_wcTEMP, aes(x=pc1, y=pc2, label=ID2)) + scale_fill_manual(values=c("red","blue","green","purple"), name="") + geom_point(aes(fill=ID1), shape=21, color="gray90", size=5) + labs(x="PC1",y="PC2")
+ggplot(data=mg_wcTEMP, aes(x=pc1, y=pc2, label=Population)) + scale_fill_manual(values=c("red","blue","green","purple"), name="") + geom_point(aes(fill=Region), shape=21, color="gray90", size=5) + labs(x="PC1",y="PC2")
 
 #save as IMG: 600x600--MAINTAIN ASPECT RATIO
+
+#####################################################################################
+#Plot with labelled variables
+#Extract loadings and create a new dataframe#
+pc1 <- mg_pcaTEMP$var$cor[,1]
+pc2 <- mg_pcaTEMP$var$cor[,2]
+
+
+load <- data.frame("var"=c("MAT", "MDR", "ISO", "TS", "MTWM", "MTCM", "TAR", "MTWQ", "MTDQ", "MTWAQ", "MTCQ"), pc1, pc2)
+load <- data.frame("var"=c("MAT", "MDR", "ISO", "TS", "MTWM", "MTCM", "TAR", "MTWQ", "MTDQ", "MTWAQ", "MTCQ"), pc1, pc2)
+
+#subset to only include important variables#
+load <- load[c("bio_1", "bio_5", "bio_6", "bio_7", "bio_9", "bio_10" ),]
+#create lengthen value for plotting loading lines#
+lengthen <-6
+
+#Final PCA with important variables plotted
+ggplot(data=mg_wcTEMP, aes(x=pc1, y=pc2)) + scale_fill_manual(values=c("red","blue"), labels=c("UK","US"), name="") + 
+  geom_point(aes(fill=Region),shape=21, color="gray90", size=6) + labs(x="PC1",y="PC2")  + geom_segment(data=load, aes(x=0, y=0, xend=pc1*lengthen, yend=pc2*lengthen), color="gray75") + 
+  geom_text(data=load, aes(x=pc1*lengthen*1.1, y=pc2*lengthen*1.1, label=var),color="black", size=5)  + theme(text=element_text(size=20), title=element_text(size=20), axis.text=element_text(size=20))
+
+#save as IMG: 900x900--MAINTAIN ASPECT RATIO
 
 #####################################################################################
 #export data
